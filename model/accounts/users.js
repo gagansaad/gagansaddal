1d0b19344d785cc09b19209eed20593d3463cf16
manomhan kumar <manomhan.kumar@netscapelabs.com>
2023-02-27 14:59:38 +0530

    feat: adding user modal

--- model/accounts/users.js
+++ model/accounts/users.js
@@ -1,20 +1,59 @@
 const mongoose = require("mongoose");
-const usersschema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },name: {
-        type: String,
-        required: true
-    },name: {
-        type: String,
-        required: true
-    }, 
 
-    userbasicInfo:{
-        name:{
-            type: struing
-        }
-    }
+const userSchema = new mongoose.Schema({
+    userBasicInfo: {
+        name: {
+            type: String,
+            required: true
+        },
+        email_address: {
+            type: String,
+            required: true,
+            index: { unique: true }
+        },
+        password: { type: String },
+        mobile_number: {
+            country_code: {
+                ...defaultStringConfig,
+            },
+            phone_number: {
+                ...defaultStringConfig,
+                match: commonRegex.mobileNumber.india,
+            }
+        },
+    },
 
-})
+    userDateInfo: {
+        loginDate: { type: Date, default: Date.now() },
+        lastLoginDate: { type: Date, default: Date.now() },
+        LastUpdate: { type: Date, default: Date.now() },
+        userDisableDate: { type: Date },
+    },
+    userFacebookInfo:{
+        fbId: { type: String },
+        fbToken: { type: String },
+        loginOn: { type: Date, default: Date.now() },
+    },
+    userGoogleInfo: {
+        googleId: { type: String },
+        googleEmail: { type: String },
+        googleToken: { type: String },
+        loginOn: { type: Date, default: Date.now() },
+    },
+
+    userAppleInfo: {
+        appleId: { type: String },
+        appleToken: { type: String },
+        loginOn: { type: Date, default: Date.now() },
+    },
+    userStatus: {
+        blocked: { type: Boolean, default: false },
+        appVersion: { type: String, default: "0.01" },
+        userActionStatus: { type: String, default: "Enable" },
+        userLoginStatusForFbGmail: { type: String, default: "SignUp" },
+        userStatus: { type: String, possibleValues: ['Login', 'Logout'] },
+    },
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('user', userSchema);
