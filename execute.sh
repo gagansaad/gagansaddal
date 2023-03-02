ad803bfbeefa10ec638b0d05f59f46e3d7f7512a
manomhan kumar <manomhan.kumar@netscapelabs.com>
2023-03-02 10:32:06 +0530

    feat: adding pipeline

---
+++ execute.sh
@@ -0,0 +1,6 @@
+cd /www/server/nginx/node/menehariya
+git pull origin development-0.0.1
+# /usr/bin/php /var/www/dev/dj-that/artisan passport:keys
+# /usr/bin/php /var/www/dev/dj-that/artisan passport:client --personal
+#/usr/bin/php /var/www/dev/dj-that/artisan db:seed
+exit
