7eeb48bea2c5ec654168db3486d559a1acab90af
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-04-05 18:03:00 +0530

    OTP STRING CHANGE

--- utils/generateOTP.js
+++ utils/generateOTP.js
@@ -1,7 +1,7 @@
 exports.generateOTP = (length) => {
     /* not using 0 as mongoose cuts it out if it's found as the first digit of a number (in this case, OTP).
     That would result in the OTP being 5 digit long, which would result in mongoose error */
-    const digits = '123456789ASDFGHJKLZXCVBNMQWER';
+    const digits = '123456789A1B2C3D4E5F6GHI7JK8LMAN';
     let OTP = ``;
 
     for (let i = 0; i < length; i++) {
