1d9612f5a60320c2600fe50b0ef28ddefc5c32ed
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-04-06 17:16:22 +0530

    delete account & update profile

--- controllers/api/accounts/user.js
+++ controllers/api/accounts/user.js
@@ -1771,8 +1771,8 @@ module.exports = {
               short_bio: user?.userBasicInfo?.short_bio || null,
               my_website: user?.userBasicInfo?.my_website || null,
               address: user?.userBasicInfo?.location?.address || null,
-              lat: user?.userBasicInfo?.location?.coordinates[0] || null,
-              long: user?.userBasicInfo?.location?.coordinates[1] || null,
+              lat: user?.userBasicInfo?.location?.coordinates?.coordinates[0] || null,
+              long: user?.userBasicInfo?.location?.coordinates?.coordinates[1] || null,
             };
             return successJSONResponse(res, { user: data });
           }
