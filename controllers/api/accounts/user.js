e12acc572760969bd3d3133fb0842a838d7e9fe3
dilmohankumar <kdilmohan101@gmail.com>
2023-04-10 13:26:55 +0530

    adding validation for same password and sending email to user when signup through social

--- controllers/api/accounts/user.js
+++ controllers/api/accounts/user.js
@@ -30,44 +30,6 @@ const {
   isValidIndianMobileNumber,
 } = require(`../../../utils/validators`);
 
-const vali = (date) => {
-  let dateformat = /^(0?[1-9]|1[0-2])[\/](0?[1-9]|[1-2][0-9]|3[01])[\/]\d{4}$/;
-
-  // Matching the date through regular expression
-  if (date.match(dateformat)) {
-    let operator = date.split("/");
-
-    // Extract the string into month, date and year
-    let datepart = [];
-    if (operator.length > 1) {
-      datepart = date.split("/");
-    }
-    let month = parseInt(datepart[0]);
-    let day = parseInt(datepart[1]);
-    let year = parseInt(datepart[2]);
-
-    // Create a list of days of a month
-    let ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if (month == 1 || month > 2) {
-      if (day > ListofDays[month - 1]) {
-        //to check if the date is out of range
-        return false;
-      }
-    } else if (month == 2) {
-      let leapYear = false;
-      if ((!(year % 4) && year % 100) || !(year % 400)) leapYear = true;
-      if (leapYear == false && day >= 29) return false;
-      else if (leapYear == true && day > 29) {
-        console.log("Invalid date format!");
-        return false;
-      }
-    }
-  } else {
-    console.log("Invalid date format!");
-    return false;
-  }
-  return "Valid date";
-};
 
 module.exports = {
   validate_signup_data: async function (req, res, next) {
@@ -214,6 +176,7 @@ module.exports = {
                 token: createJWT(result._id),
               });
 
+      
               if (result?.userInfo?.mobile_number?.phone_number) {
                 MobileNumberVerificationOTPByUserId(result._id, null);
                 console.log(`kjasjhkasdgasjgd`);
@@ -433,6 +396,11 @@ module.exports = {
               data: err,
             });
           } else {
+
+            if(result?.userInfo?.email_address){
+              WelcomeEmail(result?.userInfo?.email_address, result?.userInfo?.name)
+            }
+           
             return res.json({
               status: 201,
               data: {
@@ -631,6 +599,9 @@ module.exports = {
               data: err,
             });
           } else {
+            if(result?.userInfo?.email_address){
+              WelcomeEmail(result?.userInfo?.email_address, result?.userInfo?.name)
+            }
             return res.json({
               status: 201,
               data: {
@@ -2007,6 +1978,10 @@ module.exports = {
 
         let newPassword = req.body.newPassword;
         let oldPassword = req.body.password;
+
+        if((newPassword.trim()).toLowerCase() === (oldPassword.trim()).toLowerCase()) {
+          return failureJSONResponse(res, { message: `old password and new password can't be same` });
+        }
 
         console.log(checkUserDetail, "jcbnhchyegc");
         if (!oldPassword) {
