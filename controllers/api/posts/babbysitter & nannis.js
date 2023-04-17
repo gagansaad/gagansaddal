076df2c0c6b09b666500d2875415dc1f60cebe41
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-04-17 16:33:16 +0530

    delete user server status code change

--- controllers/api/posts/babbysitter & nannis.js
+++ controllers/api/posts/babbysitter & nannis.js
@@ -34,7 +34,7 @@ exports.getDnymicsData = async (req, res, next) => {
 ///-----------------------Validate Data---------------------------//
 
 exports.validateAdsData = async (req, res, next) => {
-    console.log(req.body.status)
+    console.log(req.body)
   try {
     const {
       status,
@@ -44,7 +44,7 @@ exports.validateAdsData = async (req, res, next) => {
      sub_type
       
     } = req.body;
-    console.log(req.body.status)
+    console.log(req.body)
     if (isNaN(Number(status))) return failureJSONResponse(res, { message: `Please enter valid status` });
     else if (status < 1 || status > 3) failureJSONResponse(res, { message: `Please enter status between 1 to 3` });
     else if (status != 1 && status != 2 &&  status != 3)  return failureJSONResponse(res, { message: `Please enter status between 1 to 3` });
@@ -122,6 +122,7 @@ console.log("isValidBoolean(hideAddress)isValidBoolean(hideAddress)isValidBoolea
 ////-----------------------Create babbysitter & nannis------------------------------//
 
 exports.createAds = async (req, res, next) => {
+  console.log(req.body);
   try {
     const {
       isfeatured,
