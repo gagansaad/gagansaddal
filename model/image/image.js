8ffc69443e8a265865f84b939d377615f606a280
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-05-01 17:37:48 +0530

    local biz

--- model/image/image.js
+++ model/image/image.js
@@ -2,11 +2,11 @@ const mongoose = require(`mongoose`);
 const MediaSchema = new mongoose.Schema({
     url: {
         type: String,
-        required: true
+       
     },
     url_type: {
         type: String,
-        required: true
+        
     }
 }, { timestamps: true });
 exports.module = mongoose.model(`media`, MediaSchema);  
