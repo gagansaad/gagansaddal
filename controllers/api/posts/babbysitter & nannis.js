725ef8facc5a94ea85992367ace8394df4156526
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-05-08 10:25:58 +0530

    buy and rvent

--- controllers/api/posts/babbysitter & nannis.js
+++ controllers/api/posts/babbysitter & nannis.js
@@ -38,8 +38,8 @@ exports.getDnymicsData = async (req, res, next) => {
     preferrd_gender_offer: ["Male", "Female"],
     transport_facilty_need: ["Yes", "No"],
     transport_facilty_offer: ["Yes", "No"],
-    expected_salary_rate_need: ["/hour", "/week", "/month", "/day"],
-    expected_salary_rate_offer: ["/hour", "/week", "/month", "/day"],
+    expected_salary_rate_need:["/hour", "/day", "/week", "/month", "/biweekly", "/sqft", "fixed amount", "OBO", "Negotiable"],
+    expected_salary_rate_offer: ["/hour", "/day", "/week", "/month", "/biweekly", "/sqft", "fixed amount", "OBO", "Negotiable"],
   };
   return successJSONResponse(res, {
     message: `success`,
