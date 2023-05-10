214df34542ce909acbe129fef0918b62b62ecd76
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-05-10 17:45:32 +0530

    plans

--- routes/api/ads/plans copy.js
+++ routes/api/ads/plans copy.js
@@ -2,7 +2,7 @@ const router = require(`express`).Router(),
     authMiddleware = require(`../../../middleware/ensureUserLoggedIn`),
     controllers = require(`../../../controllers/api/posts/plans copy`);
 
-router.post(`/`,
+router.get(`/`,
     // authMiddleware.ensureUserLoggedIn,
     controllers.fetchPlanForAds
 );
