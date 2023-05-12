a500c17f3584cc8099e6b99ad77c101279ef9604
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-05-12 19:07:30 +0530

    plans

--- controllers/api/posts/payment.js
+++ controllers/api/posts/payment.js
@@ -217,7 +217,7 @@ exports.stripe_webhooks = async   (request, response) => {
 
   try {
     event =await stripe.webhooks.constructEvent(payloadString, header,secret);
-    expect(event.id).to.equal(payload.id);
+    // expect(event.id).to.equal(payload.id);
   } catch (err) {
     response.status(400).send(`Webhook Error: ${err.message}`);
     return;
