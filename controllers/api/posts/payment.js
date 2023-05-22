08a889b0884b435493af5ab86238be4af78ba7d5
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-05-22 19:32:30 +0530

    working on stripe webhooks

--- controllers/api/posts/payment.js
+++ controllers/api/posts/payment.js
@@ -159,7 +159,7 @@ exports.create_payment_intent = async (req, res) => {
       });
       // PaymentModelInfo = await PaymentModel.findOneAndUpdate({"_id":PaymentModelId._id});
       // console.log("object,",PaymentModelInfo,"------=-===========+");
-      PaymentModelInf = await PaymentModel.findOneAndUpdate({"_id":PaymentModelId._id},{"payment_intent": paymentIntent});
+      PaymentModelInf = await PaymentModel.findByIdAndUpdate({"_id":PaymentModelId._id},{"payment_intent": "ytdtdty"});
       console.log(PaymentModelInf,"hghdegfh");
 
       paymentIntentClientSecret = paymentIntent.client_secret;
