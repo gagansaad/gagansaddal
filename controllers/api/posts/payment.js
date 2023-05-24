4f4080ba25b4348173875ac70de42befa1aa8557
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-05-24 18:52:59 +0530

    working on stripe webhooks

--- controllers/api/posts/payment.js
+++ controllers/api/posts/payment.js
@@ -198,11 +198,11 @@ exports.webhooks = async (request, response) => {
     let payment_id = event.data.object.metadata.payment_id;
     let paymentDetails = await PaymentModel.findById({ "_id":payment_id})
     const selectedKeysArray = paymentDetails?.plan_addons?.map(obj => {
-      let { amount, duration } = obj;
+      let { amount, duration,_id } = obj;
       const currentDate = new Date();
       duration=  new Date(currentDate.getTime() + (duration * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
-      let record = {amount, duration}
-      return record;
+      
+      return { amount, duration ,_id};
     });
     console.log(selectedKeysArray,"nhfnjkkndjvufnjivjs");
     // const ids = paymentDetails?.plan_addons?.map(obj => obj); 
