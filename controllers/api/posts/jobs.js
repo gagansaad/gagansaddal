2e6b226b1e0dda15d26ab539bad979532b1aa0a0
Dheeraj Gautam <dheeraj.gautam@netscapelabs.com>
2023-06-15 14:52:10 +0530

    virtual populate

--- controllers/api/posts/jobs.js
+++ controllers/api/posts/jobs.js
@@ -748,7 +748,7 @@ exports.fetchAllAds = async (req, res, next) => {
     });
    
     if (records) {
-      const jobData = jobs.map((job) => {
+      const jobData = records.map((job) => {
         return {
           _id: job._id,
           // Add other job fields as needed
