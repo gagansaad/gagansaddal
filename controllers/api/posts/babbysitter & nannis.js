d5aa279f0185dac7961aecc591d46409266948d1
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-09-01 18:34:36 +0530

    push to live

--- controllers/api/posts/babbysitter & nannis.js
+++ controllers/api/posts/babbysitter & nannis.js
@@ -667,12 +667,12 @@ exports.fetchAll = async (req, res, next) => {
     if (category_value) {
       dbQuery["adsInfo.category.category_value"] = category_value;
     }
-
-    if (category) {
+    if(category == "Im a Babysitter/Nanny"){
       dbQuery["adsInfo.category.category_name"] = category;
-    }else if(category == "I%2520want%2520a%2520Babysitter%252FNanny"){
+    }
+    else if(category == "I%2520want%2520a%2520Babysitter%252FNanny"){
       dbQuery["adsInfo.category.category_name"] = "I want a Babysitter/Nanny";
-    }else if(category == "Im a Babysitter/Nanny"){
+    }else if (category) {
       dbQuery["adsInfo.category.category_name"] = category;
     }
 
