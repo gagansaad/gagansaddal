5f8e11e1822745681f4551c612312ca02b85b827
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-09-02 12:17:18 +0530

    localbiz API admin push to live

--- controllers/admin/ads/babbysitter & nannis.js
+++ controllers/admin/ads/babbysitter & nannis.js
@@ -16,29 +16,158 @@ const mongoose = require("mongoose"),
     isValidIndianMobileNumber,
   } = require(`../../../utils/validators`);
 
-exports.fetchAll = async (req, res, next) => {
-  try {
-//     const isFeatured = req.query.isfeatured;
-//     let dbQuery ={
-//         status: 1
-//     };
-
-// if(isFeatured) dbQuery.isfeatured = isFeatured;
-      let records = await postbabyAd.find();
-      if (records) {
-          return successJSONResponse(res, {
-              message: `success`,
-              total:Object.keys(records).length,
-              records,
-              status: 200,
-          })
-      } else {
-          return failureJSONResponse(res, { message: `ad not Available` })
+  exports.fetchAll = async (req, res, next) => {
+    try {
+      let searchTerm = req.body.searchTerm;
+      let dbQuery = {};
+      const {
+        status,
+        category_value,
+        category,
+        work_type,
+        care_service,
+        age_group,
+        prefered_language,
+        prefered_gender,
+        transport_facilty,
+        location,
+        tagline,
+        sortBy,
+        longitude,
+        latitude,
+        maxDistance,
+      } = req.query;
+      console.log(req.query,"---------------");
+      const sortval = sortBy === "Oldest" ? { createdAt: 1 } : { createdAt: -1 };
+      let Distance
+      
+      if(maxDistance === "0" || !maxDistance){
+        console.log("bol");
+        Distance =  200000
+      }else{
+        Distance =maxDistance*1000
       }
-  } catch (err) {
-      return failureJSONResponse(res, { message: `something went wrong` })
-  }
-}
+      // console.log(longitude, latitude,'longitude, latitude');
+    if (longitude && latitude && Distance) {
+        const targetPoint = {
+          type: 'Point',
+          coordinates: [longitude, latitude]
+        };
+        dbQuery["adsInfo.location.coordinates"] = {
+         
+            $near: {
+              $geometry: targetPoint,
+              $maxDistance: Distance
+            }
+          
+      }
+    }
+      var perPage = parseInt(req.query.perpage) || 40;
+      var page = parseInt(req.query.page) || 1;
+  
+      if (status) {
+        dbQuery.status = status;
+      }
+  
+      if (category_value) {
+        dbQuery["adsInfo.category.category_value"] = category_value;
+      }
+  
+      if (category === "I%20want%20a%20Babysitter%2FNanny") {
+        dbQuery["adsInfo.category.category_name"] = "I want a Babysitter/Nanny";
+      }else if(category === "I%2520am%2520a%2520Babysitter%252FNanny"){
+          dbQuery["adsInfo.category.category_name"] = "I am a Babysitter/Nanny";
+      }
+      if (work_type) {
+        dbQuery["adsInfo.work_type"] = work_type;
+      }
+  
+      if (care_service) {
+        dbQuery["adsInfo.care_service"] = care_service;
+      }
+  
+      if (age_group) {
+        dbQuery["adsInfo.age_group"] = age_group;
+      }
+  
+      if (prefered_language) {
+        dbQuery["adsInfo.prefered_language"] = prefered_language;
+      }
+  
+      if (prefered_gender) {
+        dbQuery["adsInfo.prefered_gender"] = prefered_gender;
+      }
+  
+      if (transport_facilty) {
+        dbQuery["adsInfo.transport_facilty"] = transport_facilty;
+      }
+  
+      if (location) {
+        dbQuery["adsInfo.location"] = location;
+      }
+  
+      if (tagline) {
+        dbQuery["adsInfo.tagline"] = tagline;
+      }
+       // Get the current date
+       const currentDate = new Date();
+       // Convert the date to ISO 8601 format
+       const currentISODate = currentDate.toISOString();
+       // Extract only the date portion
+       const currentDateOnly = currentISODate.substring(0, 10);
+       dbQuery.status = "active";
+       dbQuery["plan_validity.expired_on"] = { $gte: currentDateOnly };
+      let queryFinal = dbQuery;
+      if (searchTerm) {
+        queryFinal = {
+          ...dbQuery,
+          $or: [
+            { "adsInfo.title": { $regex: searchTerm, $options: "i" } },
+            { "adsInfo.tagline": { $regex: searchTerm, $options: "i" } }
+          ]
+        };
+      }
+      let myid = req.userId;
+      let records = await postbabyAd
+        .find({ $or: [queryFinal] })
+        .populate({ path: "adsInfo.image", strictPopulate: false, select: "url" })
+        .populate({ path: "favoriteCount", select: "_id" })
+        .populate({ path: "viewCount" })
+        .populate({ path: 'isFavorite', select: 'user', match: { user: myid } })
+        .sort(sortval)
+        .skip(perPage * page - perPage)
+        .limit(perPage);
+        const totalCount = await postbabyAd.find({
+          $or: [queryFinal],
+        });
+        let responseModelCount = totalCount.length;
+     
+      if (records) {
+        const jobData = records.map((job) => {
+          return {
+            ...job._doc,
+            // Add other job fields as needed
+            view_count: job.viewCount,
+            favorite_count: job.favoriteCount,
+            is_favorite: !!job.isFavorite, 
+          };
+        });
+        return successJSONResponse(res, {
+          message: `success`,
+          total: responseModelCount,
+          perPage: perPage,
+          totalPages: Math.ceil(responseModelCount / perPage),
+          currentPage: page,
+          records:jobData,
+          status: 200,
+        });
+      } else {
+        return failureJSONResponse(res, { message: `ads not Available` });
+      }
+    } catch (err) {
+      return failureJSONResponse(res, { message: `something went wrong` });
+    }
+  };
 
 
 
