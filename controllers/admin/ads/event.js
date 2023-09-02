8a71c1226087c107957d135c6472c91be021fd55
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-09-02 11:47:18 +0530

    event API admin push to live

--- controllers/admin/ads/event.js
+++ controllers/admin/ads/event.js
@@ -25,27 +25,178 @@ const mongoose = require("mongoose"),
 ////////////////
 exports.fetchAll = async (req, res, next) => {
   try {
-//     const isFeatured = req.query.  ;
-//     let dbQuery ={
-//         status: 1
-//     };
+    let searchTerm = req.body.searchTerm;
+    let dbQuery = {};
+    const {
+      status,
+      title,
+      type,
+      category,
+      details,
+      recurring_type,
+      start_date,
+      end_date,
+      regular_ticket_price,
+      tagline,
+      location,
+      venue_name,
+      sortBy,
+      longitude,
+      latitude,
+      maxDistance,
+    } = req.query;
+    console.log(req.query,"--------------------------------");
+    const sortval = sortBy === "Oldest" ? { createdAt: 1 } : { createdAt: -1 };
+    // console.log(longitude, latitude,'longitude, latitude');
+    let Distance
+    if (regular_ticket_price) {
+      dbQuery["adsInfo.ticket_price.regular_ticket_price"] = {
+        $lte: parseFloat(regular_ticket_price) // Parse the input as a float if it's not already
+      };
+    }
+    
+    if (recurring_type) {
+      dbQuery["adsInfo.recurring_type"] = recurring_type;
+    }
+    
 
-// if(isFeatured) dbQuery.isfeatured = isFeatured;dbQuery
-      let records = await eventAd.find();
-      if (records) {
-          return successJSONResponse(res, {
-              message: `success`,
-              total:Object.keys(records).length,
-              records,
-              status: 200,
-          })
-      } else {
-          return failureJSONResponse(res, { message: `Room not Available` })
-      }
-  } catch (err) {
-      return failureJSONResponse(res, { message: `something went wrong` })
-  }
+if (start_date && end_date) {
+  dbQuery["adsInfo.date_time.start_date"] = {
+    $gte: start_date
+  };
+  dbQuery["adsInfo.date_time.end_date"] = {
+    $lte: end_date
+  };
+} else if (start_date) {
+  dbQuery["adsInfo.date_time.start_date"] = {
+    $gte: start_date
+  };
+} else if (end_date) {
+  dbQuery["adsInfo.date_time.end_date"] = {
+    $lte: end_date
+  };
 }
+
+
+    if(maxDistance === "0" || !maxDistance){
+      console.log("bol");
+      Distance =  200000
+    }else{
+      Distance =maxDistance*1000
+    }
+  if (longitude && latitude && Distance) {
+      const targetPoint = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      };
+      dbQuery["adsInfo.location.coordinates"] = {
+       
+          $near: {
+            $geometry: targetPoint,
+            $maxDistance: Distance
+          }
+        
+    }
+  }
+    var perPage = parseInt(req.query.perpage) || 40;
+    var page = parseInt(req.query.page) || 1;
+
+    if (status) {
+      dbQuery.status = status;
+    }
+
+    if (title) {
+      dbQuery["adsInfo.title"] = title;
+    }
+
+    if (type) {
+      dbQuery["adsInfo.type"] = type;
+    }
+
+    if (category) {
+      dbQuery["adsInfo.category"] = category;
+    }
+
+    if (details) {
+      dbQuery["adsInfo.details"] = details;
+    }
+
+    if (recurring_type) {
+      dbQuery["adsInfo.recurring_type"] = recurring_type;
+    }
+
+    if (tagline) {
+      dbQuery["adsInfo.tagline"] = tagline;
+    }
+
+    if (location) {
+      dbQuery["adsInfo.location"] = location;
+    }
+
+    if (venue_name) {
+      dbQuery["adsInfo.venue_name"] = venue_name;
+    }
+     // Get the current date
+     const currentDate = new Date();
+     // Convert the date to ISO 8601 format
+     const currentISODate = currentDate.toISOString();
+     // Extract only the date portion
+     const currentDateOnly = currentISODate.substring(0, 10);
+     dbQuery.status = "active";
+     dbQuery["plan_validity.expired_on"] = { $gte: currentDateOnly };
+
+    let queryFinal = dbQuery;
+    if (searchTerm) {
+      queryFinal = {
+        ...dbQuery,
+        $or: [
+          { "adsInfo.title": { $regex: searchTerm, $options: "i" } },
+          { "adsInfo.tagline": { $regex: searchTerm, $options: "i" } }
+        ]
+      };
+    }
+    let myid = req.userId;
+    let records = await eventAd
+      .find({ $or: [queryFinal] })
+      .populate({ path: "adsInfo.image", strictPopulate: false, select: "url" })
+      .populate({ path: "favoriteCount", select: "_id" })
+      .populate({ path: "viewCount" })
+      .populate({ path: 'isFavorite', select: 'user', match: { user: myid } })
+      .sort(sortval)
+      .skip(perPage * page - perPage)
+      .limit(perPage);
+      const totalCount = await eventAd.find({
+        $or: [queryFinal],
+      });
+      let responseModelCount = totalCount.length;
+   
+    if (records) {
+        const jobData = records.map((job) => {
+          return {
+            ...job._doc,
+            // Add other job fields as needed
+            view_count: job.viewCount,
+            favorite_count: job.favoriteCount,
+            is_favorite: !!job.isFavorite, 
+          };
+        });
+      return successJSONResponse(res, {
+        message: `success`,
+        total: responseModelCount,
+        perPage: perPage,
+        totalPages: Math.ceil(responseModelCount / perPage),
+        currentPage: page,
+        records:jobData,
+        status: 200,
+      });
+    } else {
+      return failureJSONResponse(res, { message: `Room not Available` });
+    }
+  } catch (err) {
+    return failureJSONResponse(res, { message: `something went wrong` });
+  }
+};
+
 
 exports.fetchOne = async (req, res, next) => {
   try {
