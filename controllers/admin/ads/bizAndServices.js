d239c353c5013f5b0b1224a6faf569fd1de999a3
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-09-02 12:15:06 +0530

    localbiz API admin push to live

--- controllers/admin/ads/bizAndServices.js
+++ controllers/admin/ads/bizAndServices.js
@@ -15,30 +15,163 @@ const mongoose = require("mongoose"),
     isValidEmailAddress,
     isValidIndianMobileNumber,
   } = require(`../../../utils/validators`);
-
+  
   exports.fetchAll = async (req, res, next) => {
     try {
-  //     const isFeatured = req.query.isfeatured;
-  //     let dbQuery ={
-  //         status: 1
-  //     };
-  
-  // if(isFeatured) dbQuery.isfeatured = isFeatured;dbQuery
-        let records = await  postbizAndServicesAd.find();
-        if (records) {
-            return successJSONResponse(res, {
-                message: `success`,
-                total:Object.keys(records).length,
-                records,
-                status: 200,
-            })
-        } else {
-            return failureJSONResponse(res, { message: `Room not Available` })
+      let searchTerm = req.body.searchTerm;
+      let dbQuery = {};
+      const {
+        status,
+        category,
+        sub_category,
+        business_name,
+        tagline,
+        business_location,
+        is_24_seven,
+        sortBy,
+        longitude,
+        latitude,
+        maxDistance,
+        availability,
+      } = req.query;
+      if (availability) {
+        switch (availability) {
+          case "Weekdays":
+            dbQuery["adsInfo.working_hours.week_days.is_available"] = true;
+            break;
+          case "Weekends":
+            dbQuery["adsInfo.working_hours.week_ends.is_available"] = true;
+            break;
+          case "Weekdays & Weekends":
+            dbQuery["adsInfo.working_hours.week_days.is_available"] = true;
+            dbQuery["adsInfo.working_hours.week_ends.is_available"] = true;
+            break;
+          case "24/7":
+            dbQuery["adsInfo.working_hours.is_24_seven"] = true;
+            break;
+          case "By appointment":
+            dbQuery["adsInfo.working_hours.appointment"] = true;
+            break;
+          default:
+            break;
         }
-    } catch (err) {
-        return failureJSONResponse(res, { message: `something went wrong` })
+      }
+  
+      console.log(req.query,"--------------------------------------------------------------------------------------------------------------------------------------");
+      const sortval = sortBy === "Oldest" ? { createdAt: 1 } : { createdAt: -1 };
+      // console.log(longitude, latitude,'longitude, latitude');
+      let Distance
+      
+      if(maxDistance === "0" || !maxDistance){
+        console.log("bol");
+        Distance =  200000
+      }else{
+        Distance =maxDistance*1000
+      }
+    if (longitude && latitude && Distance) {
+        const targetPoint = {
+          type: 'Point',
+          coordinates: [longitude, latitude]
+        };
+        dbQuery["adsInfo.location.coordinates"] = {
+         
+            $near: {
+              $geometry: targetPoint,
+              $maxDistance: Distance
+            }
+          
+      }
     }
-  }
+      var perPage = parseInt(req.query.perpage) || 40;
+      var page = parseInt(req.query.page) || 1;
+  
+      if (status) {
+        dbQuery.status = status;
+      }
+  
+      if (category) {
+        dbQuery.categories = category;
+      }
+  
+      if (sub_category) {
+        dbQuery.sub_categories = sub_category;
+      }
+  
+      if (business_name) {
+        dbQuery["adsInfo.title"] = business_name;
+      }
+  
+      if (tagline) {
+        dbQuery["adsInfo.tagline"] = tagline;
+      }
+  
+      if (business_location) {
+        dbQuery["adsInfo.business_location"] = business_location;
+      }
+  
+      if (is_24_seven) {
+        dbQuery["adsInfo.is_24_seven"] = is_24_seven;
+      }
+       // Get the current date
+       const currentDate = new Date();
+       // Convert the date to ISO 8601 format
+       const currentISODate = currentDate.toISOString();
+       // Extract only the date portion
+       const currentDateOnly = currentISODate.substring(0, 10);
+       dbQuery.status = "active";
+       dbQuery["plan_validity.expired_on"] = { $gte: currentDateOnly };
+      let queryFinal = dbQuery;
+      if (searchTerm) {
+        queryFinal = {
+          ...dbQuery,
+          $or: [
+            { "adsInfo.title": { $regex: searchTerm, $options: "i" } },
+            { "adsInfo.tagline": { $regex: searchTerm, $options: "i" } }
+          ]
+        };
+      }
+      let myid = req.userId;
+      let records = await postbizAndServicesAd
+        .find({ $or: [queryFinal] })
+        .populate({ path: "adsInfo.image", strictPopulate: false, select: "url" })
+        .populate({ path: "favoriteCount", select: "_id" })
+        .populate({ path: "viewCount" })
+        .populate({ path: 'isFavorite', select: 'user', match: { user: myid } })
+        .sort(sortval)
+        .skip(perPage * page - perPage)
+        .limit(perPage);
+        const totalCount = await postbizAndServicesAd.find({
+          $or: [queryFinal],
+        });
+        let responseModelCount = totalCount.length;
+     
+      if (records) {
+        const jobData = records.map((job) => {
+          return {
+            ...job._doc,
+            // Add other job fields as needed
+            view_count: job.viewCount,
+              favorite_count: job.favoriteCount,
+              is_favorite: !!job.isFavorite, 
+          };
+        });
+        return successJSONResponse(res, {
+          message: `success`,
+          total: responseModelCount,
+          perPage: perPage,
+          totalPages: Math.ceil(responseModelCount / perPage),
+          currentPage: page,
+          records:jobData,
+          status: 200,
+        });
+      } else {
+        return failureJSONResponse(res, { message: `ads not Available` });
+      }
+    } catch (err) {
+      return failureJSONResponse(res, { message: `something went wrong` });
+    }
+  };
+  
 
   exports.fetchOne = async (req, res, next) => {
     try {
