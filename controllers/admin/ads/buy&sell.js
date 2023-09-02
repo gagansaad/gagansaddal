6f9acc407121093d2009dc56d14ad27d69942b74
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-09-02 12:09:43 +0530

    event API admin push to live

--- controllers/admin/ads/buy&sell.js
+++ controllers/admin/ads/buy&sell.js
@@ -23,29 +23,159 @@ postBuySellAd = mongoose.model("Buy & Sell"),
 /////////////
 
 
+
 exports.fetchAll = async (req, res, next) => {
   try {
-//     const isFeatured = req.query.isfeatured;
-//     let dbQuery ={
-//         status: 1
-//     };
-
-// if(isFeatured) dbQuery.isfeatured = isFeatured;dbQuery
-      let records = await postBuySellAd.find();
-      if (records) {
-          return successJSONResponse(res, {
-              message: `success`,
-              total:Object.keys(records).length,
-              records,
-              status: 200,
-          })
-      } else {
-          return failureJSONResponse(res, { message: `Room not Available` })
-      }
-  } catch (err) {
-      return failureJSONResponse(res, { message: `something went wrong` })
+    let searchTerm = req.body.searchTerm;
+    let dbQuery = {};
+    const {
+      status,
+      ads_type,
+      category,
+      sub_category,
+      title,
+      product_condition,
+      user_type,
+      payment_mode,
+      fullfilment,
+      location,
+      tagline,
+      sortBy,
+      longitude,
+      latitude,
+      maxDistance,
+    } = req.query;
+    const sortval = sortBy === "Oldest" ? { createdAt: 1 } : { createdAt: -1 };
+    // console.log(longitude, latitude,'longitude, latitude');
+    let Distance
+    
+    if(maxDistance === "0" || !maxDistance){
+      console.log("bol");
+      Distance =  200000
+    }else{
+      Distance =maxDistance*1000
+    }
+  if (longitude && latitude && Distance) {
+      const targetPoint = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      };
+      dbQuery["adsInfo.location.coordinates"] = {
+       
+          $near: {
+            $geometry: targetPoint,
+            $maxDistance: Distance
+          }
+        
+    }
   }
-}
+    var perPage = parseInt(req.query.perpage) || 40;
+    var page = parseInt(req.query.page) || 1;
+    if (status) {
+      dbQuery.status = status;
+    }
+
+    if (ads_type) {
+      dbQuery.adsType = ads_type;
+    }
+
+    if (category) {
+      dbQuery["adsInfo.category"] = category;
+    }
+
+    if (sub_category) {
+      dbQuery["adsInfo.sub_category"] = sub_category;
+    }
+
+    if (title) {
+      dbQuery["adsInfo.title"] = title;
+    }
+
+    if (product_condition) {
+      dbQuery["adsInfo.product_condition"] = product_condition;
+    }
+
+    if (user_type) {
+      dbQuery["adsInfo.user_type"] = user_type;
+    }
+
+    if (payment_mode) {
+      dbQuery["adsInfo.payment_mode"] = payment_mode;
+    }
+
+    if (fullfilment) {
+      dbQuery["adsInfo.fullfilment"] = fullfilment;
+    }
+
+    if (location) {
+      dbQuery["adsInfo.location"] = location;
+    }
+
+    if (tagline) {
+      dbQuery["adsInfo.tagline"] = tagline;
+    }
+     // Get the current date
+     const currentDate = new Date();
+     // Convert the date to ISO 8601 format
+     const currentISODate = currentDate.toISOString();
+     // Extract only the date portion
+     const currentDateOnly = currentISODate.substring(0, 10);
+     dbQuery.status = "active";
+     dbQuery["plan_validity.expired_on"] = { $gte: currentDateOnly };
+console.log(dbQuery,"77777777777777777777777777777777777777777777777");
+    let queryFinal = dbQuery;
+    if (searchTerm) {
+      queryFinal = {
+        ...dbQuery,
+        $or: [
+          { "adsInfo.title": { $regex: searchTerm, $options: "i" } },
+          { "adsInfo.tagline": { $regex: searchTerm, $options: "i" } }
+        ]
+      };
+    }
+    let myid = req.userId;
+    let records = await postBuySellAd
+      .find({ $or: [queryFinal] })
+      .populate({ path: "adsInfo.image", strictPopulate: false, select: "url" })
+      .populate({ path: "favoriteCount", select: "_id" })
+      .populate({ path: "viewCount" })
+      .populate({ path: 'isFavorite', select: 'user', match: { user: myid } })
+      .sort(sortval)
+      .skip(perPage * page - perPage)
+      .limit(perPage);
+      const totalCount = await postBuySellAd.find({
+        $or: [queryFinal],
+      });
+      let responseModelCount = totalCount.length;
+   
+    if (records) {
+      const jobData = records.map((job) => {
+        return {
+          ...job._doc,
+          // Add other job fields as needed
+          view_count: job.viewCount,
+          favorite_count: job.favoriteCount,
+          is_favorite: !!job.isFavorite, 
+        };
+      });
+      return successJSONResponse(res, {
+        message: `success`,
+        total: responseModelCount,
+        perPage: perPage,
+        totalPages: Math.ceil(responseModelCount / perPage),
+        currentPage: page,
+        records:jobData,
+        status: 200,
+      });
+    } else {
+      return failureJSONResponse(res, { message: `ads not Available` });
+    }
+  } catch (err) {
+    console.log(err);
+    return failureJSONResponse(res, { message: `something went wrong` });
+  }
+};
+
 
 
 exports.fetchOne = async (req, res, next) => {
