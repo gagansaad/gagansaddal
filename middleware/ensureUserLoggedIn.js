b363eb540ff10409757d7c675aee4bd05274a8dd
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-09-08 18:51:57 +0530

    clients API admin push to live

--- middleware/ensureUserLoggedIn.js
+++ middleware/ensureUserLoggedIn.js
@@ -19,7 +19,7 @@ exports.ensureUserLoggedIn = async (req, res, next) => {
 
         console.log(`token`, token)
 
-        if (!token) {return failureJSONResponse(res,
+        if (!token) {return failureJSONResponse(401,
             {message: `InvalidToken` },
             
         ) }
@@ -57,7 +57,7 @@ exports.ensureUserLoggedIn = async (req, res, next) => {
         return next();
     } catch (err) {
         console.log(err,"vdvdvdvdvd")
-        return failureJSONResponse(res,
+        return failureJSONResponse(401,
             {message: `InvalidToken` },
             
         ) 
