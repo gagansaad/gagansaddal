0d89a96f4ba6e97e2fae6d6c38a6192e4db7fe3a
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-10-25 12:45:46 +0530

    Date 24 oct dushera 2023 push

--- controllers/api/posts/fectchAllMyAds.js
+++ controllers/api/posts/fectchAllMyAds.js
@@ -913,10 +913,4 @@ exports.search = async (req, res, next) => {
 };
 
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
 
