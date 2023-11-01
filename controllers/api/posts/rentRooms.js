ebef74ec0bc73240b8f4b182de729364e1ae757b
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-11-01 17:59:40 +0530

    Date 24 oct dushera 2023 push

--- controllers/api/posts/rentRooms.js
+++ controllers/api/posts/rentRooms.js
@@ -43,7 +43,9 @@ const { mongoose, ObjectId, modelNames } = require("mongoose"),
       let updateFields = {
         $set: {
           status: "deleted",
-          deletedAt: new Date().toISOString()// Add your temporary field and its value here
+          deletedAt: new Date().toISOString(),// Add your temporary field and its value here
+          "plan_validity.expired_on": new Date().toISOString(), // Set the plan validity expiry date
+          "addons_validity.$[].expired_on": new Date().toISOString(), 
         }
       };
   
