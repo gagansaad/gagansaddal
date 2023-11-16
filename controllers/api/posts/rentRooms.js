a83e2d8b8119ad64c0d8f370beff631ff277b353
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-11-16 17:03:27 +0530

    Date 24 oct dushera 2023 push

--- controllers/api/posts/rentRooms.js
+++ controllers/api/posts/rentRooms.js
@@ -961,14 +961,16 @@ exports.fetchAll = async (req, res, next) => {
     let queryFinal = dbQuery;
     if (searchTerm) {
       adOnsQuery.$or = [
-        { "adsInfo.title": { $regex: `^${searchTerm.trim()}`, $options: "i" } },
+        // { "adsInfo.title": { $regex: `^${searchTerm.trim()}`, $options: "i" } },
+        { "adsInfo.title": { $regex: searchTerm.trim(), $options: "i" } },
+
         { "advertisement_id": searchTerm },
       ];
       queryFinal = {
         ...dbQuery,
         $or: [
-          // { "adsInfo.title": { $regex: searchTerm.trim(), $options: "i" } },
-          { "adsInfo.title": { $regex: `^${searchTerm.trim()}`, $options: "i" } },
+          { "adsInfo.title": { $regex: searchTerm.trim(), $options: "i" } },
+          // { "adsInfo.title": { $regex: `^${searchTerm.trim()}`, $options: "i" } },
           // { "adsInfo.tagline": { $regex: searchTerm.trim(), $options: "i" } },
           {"advertisement_id":searchTerm}
         ],
