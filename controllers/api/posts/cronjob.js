4709b949c5b6b7717f0a065bb207a5e0fdc5a09f
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-12-01 12:38:29 +0530

    Date 24 oct dushera 2023 push

--- controllers/api/posts/cronjob.js
+++ controllers/api/posts/cronjob.js
@@ -6,7 +6,7 @@ const {
   ModelNameByAdsType,
 } = require(`../../../handlers/jsonResponseHandlers`);
 
-cron.schedule("*/2 * * * *", async () => {
+cron.schedule("*/1 * * * *", async () => {
   try {
     function formatDate(date) {
       const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
