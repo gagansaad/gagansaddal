21762c09dc913c77228e28d86c92f27399865a92
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-12-07 14:46:30 +0530

    Date  2023 push

--- app.js
+++ app.js
@@ -372,7 +372,7 @@ try {
 let count = 0;
 
 chatting.messages.map((message) => {
-  console.log(message.senderId._id.toString() !== senderId.toString() && message.status === "unseen");
+  console.log(message.senderId._id.toString() !== senderId.toString() && message.status === "unseen",message.senderId._id.toString() ,senderId.toString());
   if (message.senderId._id.toString() !== senderId.toString() && message.status === "unseen") {
       count++;
   }
