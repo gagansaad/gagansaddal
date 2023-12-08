d3b015f4ed1a0562bd53028bee905c2d3480dcc5
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2023-12-08 16:32:17 +0530

    Date 24 oct dushera 2023 push

--- model/posts/event.js
+++ model/posts/event.js
@@ -224,13 +224,7 @@ events_Schema.virtual('expiredAt').get(function () {
 });
 
 events_Schema.virtual('active_on_virtual').get(function () {
-  // Check if active_on_bumpup_at is not null
-  if (this.active_on_bumpup_at !== null) {
-    return this.active_on_bumpup_at;
-  }
-  if (this.active_on_bumpup_at == null) {
-    return this.plan_validity.active_on;
-  }
+
   // Check if addons_validity is not empty
   if (this.plan_validity && this.plan_validity.active_on) {
     return this.plan_validity.active_on;
