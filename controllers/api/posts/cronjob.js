464d638887b5641a6cc904d6d3f0b38dc798cc07
Gagandeep singh <gagandeep.singh@netscapelabs.com>
2024-01-10 11:43:43 +0530

    D

--- controllers/api/posts/cronjob.js
+++ controllers/api/posts/cronjob.js
@@ -220,8 +220,9 @@ cron.schedule("*/1 * * * *", async () => {
 
           const currentTimeInTimeZone = new Date().toLocaleString('en-US', { timeZone: data?.location_timezone });
           const currentHour = new Date(currentTimeInTimeZone).getHours();
+          console.log(currentHour,"ddmdn");
 //hello
-          if (currentHour === 7 && bumpUpAddon) {
+          if (currentHour === 0 && bumpUpAddon) {
             const iter = bumpUpAddon.days == 30 ? 1 : bumpUpAddon.days;
             return {
               id: data._id,
